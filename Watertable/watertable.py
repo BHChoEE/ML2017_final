104a59da6169758c205ad128903df2a6f1752747
Kuan Tung <b03901039@ntu.edu.tw>
2017-05-19 23:40:04 +0800

    Linted (using pylint)

--- Watertable/watertable.py
+++ Watertable/watertable.py
@@ -1,90 +1,94 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+"""
+Predict the operating condition of waterpoints
+"""
 
 import os
-import argparse
 import csv
 import numpy as np
 from sklearn.feature_extraction import DictVectorizer
-#from keras.preprocessing.text import Tokenizer
-#from sklearn.preprocessing import MultiLabelBinarizer
-
-
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-MODEL_DIR = os.path.join(BASE_DIR, 'model')
-if not os.path.exists(MODEL_DIR):
-    os.makedirs(MODEL_DIR)
 
-if __name__ == '__main__':
-    # get the data id and label
-    labelfile = os.path.join(BASE_DIR, "label.csv")
-    labeldict = {'functional':0,'non functional':1,'functional needs repair':2}
+def main():
+    """ Main function """
+    # Get the data id and label
+    label_file = os.path.join(BASE_DIR, 'data/train_labels.csv')
+    label_dict = {'functional': 0, 'non functional': 1,
+                  'functional needs repair': 2}
     id_train = []
-    y_train=[]
-    with open(labelfile,'r') as f:
-        for row in csv.DictReader(f):
-            y_train.append(labeldict[row['status_group']])
+    y_train = []
+    with open(label_file, 'r') as lab_file:
+        for row in csv.DictReader(lab_file):
+            y_train.append(label_dict[row['status_group']])
             id_train.append(int(row['id']))
 
     y_train = np.array(y_train)
     id_train = np.array(id_train)
 
-    # get the feature
+    # Get the feature
     # Feature listed in here is useless in my opinion or similar to other
-    useless_fea = ['id','funder','installer','region','recorded_by','extraction_type_group',
-                   'payment_type','quantity_group','source_type','waterpoint_type_group',
-                   'wpt_name','subvillage']
-    datafile = os.path.join(BASE_DIR, "train.csv")
+    useless_features = ['id', 'funder', 'installer', 'region', 'recorded_by',
+                        'extraction_type_group', 'payment_type', 'quantity_group',
+                        'source_type', 'waterpoint_type_group', 'wpt_name', 'subvillage']
+    data_file = os.path.join(BASE_DIR, 'data/train_values.csv')
     data = []
-    with open(datafile,'r') as f:
-        for i,row in enumerate(csv.reader(f)):
-            #feature_type = list(row.keys())
-            #break
+    with open(data_file, 'r') as dat_file:
+        for i, row in enumerate(csv.reader(dat_file)):
             if i == 0:
                 feature_type = row
-                featuredict = {feature_type[k]:k for k in range(len(feature_type))}
-                for key in useless_fea:
-                    del featuredict[key]
-                #print(featuredict)
+                feature_dict = {
+                    feature_type[k]: k for k in range(len(feature_type))}
+                for key in useless_features:
+                    del feature_dict[key]
             else:
-                # data is a list with each element is a dictionary
-                # feature can be accessed with data[i][feature]
-                data.append({key:row[value]
-                     for key,value in featuredict.items()})
-    datasize = len(data)
+                # Data is a list with each element is a dictionary
+                # Feature can be accessed with data[i][feature]
+                data.append({key: row[value]
+                             for key, value in feature_dict.items()})
+
+    data_size = len(data)
 
     # Feature listed in here should be normalized
-    continuous_fea = ['amount_tsh','date_recorded','gps_height','longitude',
-                      'latitude','num_private','population','construction_year']
+    continuous_features = ['amount_tsh', 'date_recorded', 'gps_height', 'longitude',
+                           'latitude', 'num_private', 'population', 'construction_year']
 
     # date_recorded should be transformed to the days since it has been recorded
-    for i in range(datasize):
+    for i in range(data_size):
         year = int(data[i]['date_recorded'][:4])
         month = int(data[i]['date_recorded'][5:7])
         date = int(data[i]['date_recorded'][8:10])
-        day = (30-date) + 30*(12-month) + 365*(2017-year)
+        day = (30 - date) + 30 * (12 - month) + 365 * (2017 - year)
         data[i]['date_recorded'] = day
 
     # Normalization
-    temparray = [ [ data[i][feature] for feature in continuous_fea ]
-                  for i in range(datasize) ]
-    temparray = np.array(temparray,dtype=float)
-    mean = np.mean(temparray,axis=0)
-    std = np.std(temparray,axis=0)
-    normData = (temparray - mean) / std # this array can be then concatenate with one-hot encoded discrete data
+    tmp = [[data[i][feature] for feature in continuous_features]
+           for i in range(data_size)]
+    tmp = np.array(tmp, dtype=float)
+    mean = np.mean(tmp, axis=0)
+    std = np.std(tmp, axis=0)
+    # This array can be then concatenate with one-hot encoded discrete data
+    norm_data = (tmp - mean) / std
 
     # Other Feature is discrete and should be dealed with one-hot encoding
-    discrete_fea = ['basin','region_code','district_code','lga','ward','public_meeting',
-                    'scheme_management','scheme_name','permit','extraction_type',
-                    'extraction_type_class','management','management_group','payment',
-                    'water_quality','quality_group','quantity','source','source_class',
-                    'waterpoint_type']
+    discrete_features = ['basin', 'region_code', 'district_code', 'lga', 'ward', 'public_meeting',
+                         'scheme_management', 'scheme_name', 'permit', 'extraction_type',
+                         'extraction_type_class', 'management', 'management_group', 'payment',
+                         'water_quality', 'quality_group', 'quantity', 'source', 'source_class',
+                         'waterpoint_type']
 
-    for feature in discrete_fea:
-
-        # temp is a list of dictionary. Each dictionary only contains 1 kind feature.
-        temp = [ { feature : data[i][feature] } for i in range(datasize) ]
+    for feature in discrete_features:
+        # Temp is a list of dictionary. Each dictionary only contains 1 kind of feature
+        tmp = [{feature: data[i][feature]} for i in range(data_size)]
         vec = DictVectorizer()
-        data_array = vec.fit_transform(temp).toarray() # can be concatenate to x_train, not yet concatenate
-        data_feature = vec.get_feature_names() # can be concatenate to the featurelabel
-        print('The size of {}:  '.format(feature),end='')
+        # Can be concatenate to x_train, not yet concatenate
+        data_array = vec.fit_transform(tmp).toarray()
+        # Can be concatenate to the feature labels
+        data_feature = vec.get_feature_names()
+        print('The size of {}: '.format(feature), end='')
         print(data_array.shape)
+
+if __name__ == '__main__':
+
+    main()
